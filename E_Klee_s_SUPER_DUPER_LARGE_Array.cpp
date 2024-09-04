305e519b3f36d74e1b653c51e15147668ceae010
madhulagg <f20221236@goa.bits-pilani.ac.in>
2024-09-05 01:12:36 +0530

    new question

---
+++ E_Klee_s_SUPER_DUPER_LARGE_Array.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+//Madhul_Aggarwal
+
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+const lld pi = 3.141592653589793238;
+const ll INF = LONG_LONG_MAX;
+const ll mod = 1e9 + 7;
+
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i, m, n) for (int i = m; i < n; i++)
+#define rl(i, m, n) for (int i = m; i >= n; i--)
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pn cout << "NO\n";
+#define vr(v) v.begin(), v.end()
+#define rv(v) v.end(), v.begin()
+#define FAST ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    int prefix_sum[n];
+    int s_sum[n];
+    prefix_sum[0] = 0;
+
+    fl(i, 0, n){
+        a[i] = k + i;
+        if(i > 0) prefix_sum[i] = prefix_sum[i-1] + a[i-1];
+    }
+
+
+}
+
+ 
+signed main(){
+    FAST;
+    int testcase;
+    cin>>testcase;
+    // testcase = 1;
+    while(testcase--){
+        solve();
+    }
+}
