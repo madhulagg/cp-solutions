5d6a4052ee0b789f481ded8b5afc6cbc95aac4ba
madhulagg <f20221236@goa.bits-pilani.ac.in>
2024-09-11 18:44:00 +0530

    “update”

--- A_Sereja_and_Dima.cpp
+++ A_Sereja_and_Dima.cpp
@@ -34,13 +34,37 @@ typedef pair<int, int> pii;
 #define rv(v) v.end(), v.begin()
 #define FAST ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 
-void solve(){
+void solve() {
     int n;
     cin >> n;
-    int arr[n];
-    cin >> n;
-    fl(i, 0, n) cin >> arr[i];
     
+    vector<int> cards(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> cards[i];
+    }
+
+    int left = 0, right = n - 1;
+    int sereja = 0, dima = 0;
+    bool serejaTurn = true;
+
+    while (left <= right) {
+        if (cards[left] > cards[right]) {
+            if (serejaTurn)
+                sereja += cards[left];
+            else
+                dima += cards[left];
+            left++;
+        } else {
+            if (serejaTurn)
+                sereja += cards[right];
+            else
+                dima += cards[right];
+            right--;
+        }
+        serejaTurn = !serejaTurn; 
+    }
+
+    cout << sereja << " " << dima << endl;
 }
 
  
